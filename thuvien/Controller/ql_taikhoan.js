f4ad550136f46c67198363844a1bcbb2e82481d6
Hutramikha <minhkhang28042004@gmail.com>
2024-11-26 19:09:16 +0700

    ne

---
+++ thuvien/Controller/ql_taikhoan.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    /* Các nút hành động */
+    const btn_delete = document.querySelector('.btn-delete-taikhoan');
+    const btn_edit = document.querySelector('.btn-edit-taikhoan');
+    const btn_save = document.querySelector('.btn-save-taikhoan');
+    const btn_cancel = document.querySelector('.btn-cancel-taikhoan');
+    
+    // btn_add.addEventListener('click', () => {
+    //     btn_delete.disabled = true;
+    //     btn_edit.disabled = true;
+    //     btn_save.disabled = false;
+    //     btn_cancel.disabled = false;  
+    // });
+    
+    btn_edit.addEventListener('click', () => {
+        // btn_add.disabled = true;
+        btn_delete.disabled = true; 
+        btn_save.disabled = false;
+        btn_cancel.disabled = false;  
+    });
+    
+    btn_delete.addEventListener('click', () => {
+        
+    });
+    
+    });

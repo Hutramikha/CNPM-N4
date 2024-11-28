f4833509a5ed451225eac11574c3512646dfa839
Hutramikha <minhkhang28042004@gmail.com>
2024-11-28 23:06:55 +0700

    ad

--- thuvien/Controller/ql_phieunhap.js
+++ thuvien/Controller/ql_phieunhap.js
@@ -1,14 +1,91 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     /* Các nút hành động */
-    const btn_add_ct = document.querySelector('.btn-start-ct-phieu_nhap');
-    const btn_save = document.querySelector('.btn-add-ct-phieu_nhap');
+    const btn_start_add_ct = document.querySelector('.btn-start-ct-phieu_nhap');
+    const btn_add_ct = document.querySelector('.btn-add-ct-phieu_nhap');
     const btn_cancel = document.querySelector('.btn-cancel-ct-phieu_nhap');
+    const btn_reset = document.querySelector('.btn-reset-phieu_nhap');
+
+    const btn_add_pn = document.querySelector('.btn-create-pn');
+
+    const input_search = document.querySelector('.search-input-phieu_nhap');
+
+    const select_ten_sach_pn = document.querySelector('.select-ten_sach_pn');
+    const input_soluong_sach_pn = document.querySelector('.input-soluong_sach_pn');
+    const input_gianhap_sach_pn = document.querySelector('.input-gianhap_sach_pn');
+    const input_thanhtien_sach_pn = document.querySelector('.input-thanhtien_sach_pn');
+
+    const select_ncc_sach_pn = document.querySelector('.select-ncc_sach_pn');
+
+    const table_ct_phieu_nhap_xem = document.querySelector('.table-ct-phieu_nhap-xem');
+    const table_ct_phieu_nhap = document.querySelector('.table-ct-phieu_nhap');
     
-    btn_add_ct.addEventListener('click', () => {
+    function ableInput() {
+        select_ten_sach_pn.disabled = false;
+        input_soluong_sach_pn.disabled = false;
+        input_gianhap_sach_pn.disabled = false;
+        input_thanhtien_sach_pn.disabled = false;
+        select_ncc_sach_pn.disabled = false;
+    }
+
+    function disableInput() {
+        select_ten_sach_pn.disabled = true;
+        input_soluong_sach_pn.disabled = true;
+        input_gianhap_sach_pn.disabled = true;
+        input_thanhtien_sach_pn.disabled = true;
+        select_ncc_sach_pn.disabled = true;
+    }
+
+    function resetInput() {
+        input_soluong_sach_pn.value = '';
+        input_gianhap_sach_pn.value = '';
+        input_thanhtien_sach_pn.value = '';
+    
+        select_ten_sach_pn.selectedIndex = 0;
+        select_ncc_sach_pn.selectedIndex = 0;
+    }
+
+    btn_start_add_ct.addEventListener('click', () => {
+        btn_start_add_ct.disabled = true;
+        btn_add_ct.disabled = false;
+        btn_cancel.disabled = false;
+
+        btn_add_pn.disabled = false;
+
+        table_ct_phieu_nhap_xem.style.display = 'none';
+        table_ct_phieu_nhap.style.display = 'table';
+        
+        ableInput();
+    });
+
+    btn_cancel.addEventListener('click', () => {
+        btn_start_add_ct.disabled = false;
         btn_add_ct.disabled = true;
-        btn_save.disabled = false;
-        btn_cancel.disabled = false;  
+        btn_cancel.disabled = true;
+
+        btn_add_pn.disabled = true;
+
+        table_ct_phieu_nhap_xem.style.display = 'table';
+        table_ct_phieu_nhap.style.display = 'none';
+        
+        resetInput();
+        disableInput();
+    });
+
+    btn_reset.addEventListener('click', () => {
+        btn_start_add_ct.disabled = false;
+        btn_add_ct.disabled = true;
+        btn_cancel.disabled = true;
+
+        btn_add_pn.disabled = true;
+
+        input_search.value = '';
+
+        table_ct_phieu_nhap_xem.style.display = 'table';
+        table_ct_phieu_nhap.style.display = 'none';
+        
+        resetInput();
+        disableInput();
     });
     
     
--- thuvien/Controller/ql_taikhoan.js
+++ thuvien/Controller/ql_taikhoan.js
@@ -5,23 +5,71 @@ document.addEventListener("DOMContentLoaded", () => {
     const btn_edit = document.querySelector('.btn-edit-taikhoan');
     const btn_save = document.querySelector('.btn-save-taikhoan');
     const btn_cancel = document.querySelector('.btn-cancel-taikhoan');
+    const btn_reset = document.querySelector('.btn-reset-taikhoan');
     
-    // btn_add.addEventListener('click', () => {
-    //     btn_delete.disabled = true;
-    //     btn_edit.disabled = true;
-    //     btn_save.disabled = false;
-    //     btn_cancel.disabled = false;  
-    // });
+    const input_search = document.querySelector('.search-input-taikhoan');
+
+    const input_username_tk = document.querySelector('.input-username_tk');
+    const input_password_tk = document.querySelector('.input-password_tk');
+    const select_quyen_tk = document.querySelector('.select-quyen_tk');
+    const input_ngaytao_tk = document.querySelector('.input-ngaytao_tk');
+
+    function ableInput() {
+        // input_username_tk.disabled = false;
+        // input_password_tk.disabled = false;
+        select_quyen_tk.disabled = false;
+        // input_ngaytao_tk.disabled = false;
+    }
+
+    function disableInput() {
+        // input_username_tk.disabled = true;
+        // input_password_tk.disabled = true;
+        select_quyen_tk.disabled = true;
+        // input_ngaytao_tk.disabled = true;
+    }
+
+    function resetInput() {
+        input_username_tk.value = '';
+        input_password_tk.value = '';
+        input_ngaytao_tk.value = '';
+    
+        select_quyen_tk.selectedIndex = 0;
+    }
     
     btn_edit.addEventListener('click', () => {
         // btn_add.disabled = true;
         btn_delete.disabled = true; 
         btn_save.disabled = false;
-        btn_cancel.disabled = false;  
+        btn_cancel.disabled = false;
+        
+        ableInput();
     });
     
     btn_delete.addEventListener('click', () => {
         
+    });
+
+    btn_cancel.addEventListener('click', () => {
+        // btn_add.disabled = true;
+        btn_delete.disabled = false; 
+        btn_save.disabled = true;
+        btn_cancel.disabled = true;
+        
+        resetInput();
+        disableInput();
+    });
+
+    btn_reset.addEventListener('click', () => {
+        input_search.value = '';
+
+        // btn_add.disabled = true;
+        btn_edit.disabled = false;
+        btn_delete.disabled = false; 
+        btn_save.disabled = true;
+        btn_cancel.disabled = true;
+        
+        resetInput();
+        disableInput();
     });
     
     });
